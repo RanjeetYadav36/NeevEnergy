6be72a4b1f44e79c8bebfd62963e18a4814e11b0
Ranjeet Yadav <ranjeet.yadav@innobitsystems.com>
2023-02-14 18:32:58 +0530

    backendUpload

---
+++ src/main/java/com/innobitsystems/neev/NeevApplication.java
@@ -0,0 +1,13 @@
+package com.innobitsystems.neev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/innobitsystems/neev/NeevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innobitsystems.neev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
